46f6937330ceba16df5d12f258a377ac072e8234
rahuls79 <rahul.rahulr.sinha@gmail.com>
2022-12-14 20:21:51 +0530

    new commit

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -3,42 +3,16 @@ import "./ToDoList.css";
 import cross from "../images/icon-cross.svg";
 import ToDo from './ToDo';
 
-function ToDoList({todos,setTodos,title,set}) {
+function ToDoList({todos,setTodos}) {
   
-    // const addToList = () =>{
-    //    setTodos([...todos,{id : Math.random().toString(), value : todos.title, status : false}]);
-    //    console.log(todos)
-    // }
-
-    // const [title,setTitle] = useState((todos.title));
 
 
     const removeItemHandler = (id) =>{
-        // setKey(key);
-
-        // var l = todos.length-1;
-        // console.log(l)
-        // var id = todos[l].id;
-        // console.log(id)
         const updatedList = todos.filter((l) => l.id !== id)
         console.log(updatedList);
         setTodos([...updatedList]);
     }
     
-//     const checkboxClickHandler = (title,id) =>{
-
-//         var updatedTitle = todos.filter(l => {
-//             if(l.id === id) { // checking just the category, but you can check if any of more fields contains the conditions 
-//                 l.title = title + "hi";
-//             }
-//         // const updatedTitle = todos.filter((l) => 
-//         // l.title = title + "hi")
-//         // setTitle(title + "hello");
-//         console.log(updatedTitle);
-//         // setTitle(updatedTitle);
-        
-//     })
-// }
 
     return (
     <div>
